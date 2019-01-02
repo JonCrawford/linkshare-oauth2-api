c8f744e42b3cce1622cfc8ed54c1e0777ab57788
Jonathan Crawford <mail@joncrawford.com>
2019-01-02 14:51:36 -0800

    Properly encode dates in format expected by API
    The built-in Rails #to_query method encodes spaces as `%20` which breaks
    is not accepted by the Linkshare API and returns an "Invalid Date" error
    message.

--- lib/linkshare/events.rb
+++ lib/linkshare/events.rb
@@ -1,7 +1,7 @@
 module Linkshare
   class Events
 
-    # 
+    #
     # Description of #getEvents
     #
     # @param params [Hash] default: {}
@@ -9,8 +9,14 @@ module Linkshare
   	def getEvents(params = {})
       base_url = Linkshare::API_URIS[:events]
       params.reverse_merge!(limit: 1000, page: 1)
-      url = "#{base_url}?#{params.to_query}"
-  		Linkshare::Response.json_parse(Linkshare::Request.get(url, {:headers => {'Accept' => 'text/json'}}))
-  	end
+      [:process_date_start, :process_date_end, :transaction_date_start, :transaction_date_end].each do |param|
+        next unless params.has_key?(param)
+
+        params[param] = [DateTime.parse(params[param]).strftime("%F %T")].join
+      end
+      param_str = params.map{|x| "#{x[0]}=#{x[1]}"}.join('&')
+      url = [base_url, '?', param_str].join
+      Linkshare::Response.json_parse(Linkshare::Request.get(url, {:headers => {'Accept' => 'text/json'}}))
+    end
   end
 end
