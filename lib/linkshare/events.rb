cb9c6977535546f83970e7ac235a593e89c3524f
Jonathan Crawford <mail@joncrawford.com>
2019-01-02 14:12:20 -0800

    Respect params sent to #getEvents.

--- lib/linkshare/events.rb
+++ lib/linkshare/events.rb
@@ -1,7 +1,16 @@
 module Linkshare
   class Events
-  	def getEvents(process_date_start = nil, process_date_end=nil, transaction_date_start=nil, transaction_date_end=nil, limit="1000", page="1")
-  		Linkshare::Response.json_parse(Linkshare::Request.get(Linkshare::API_URIS[:events], {:headers => {'Accept' => 'text/json'}}))
+
+    # 
+    # Description of #getEvents
+    #
+    # @param params [Hash] default: {}
+    # @return [Array] Array of hashes containing Event data.
+  	def getEvents(params = {})
+      base_url = Linkshare::API_URIS[:events]
+      params.reverse_merge!(limit: 1000, page: 1)
+      url = "#{base_url}?#{params.to_query}"
+  		Linkshare::Response.json_parse(Linkshare::Request.get(url, {:headers => {'Accept' => 'text/json'}}))
   	end
   end
 end
